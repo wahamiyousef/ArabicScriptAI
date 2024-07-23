87218b3b3d05fcd7b1bb4ae61f0a47fe3b382e3b
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2024-07-23 16:52:08 -0400

    CRA

---
+++ server/functions.py
@@ -0,0 +1,25 @@
+import json
+from random import randint
+
+
+
+with open("arabic_alphabet.json", encoding="UTF-8") as f:
+  data = json.load(f)
+'''
+f = open('arabic_alphabet.json', encoding="UTF-8")
+data = json.load(f)
+'''
+
+def get_random_letter():
+  #random_let = random.randint(0,len(data))
+  #random_let = data["alphabet"]
+  #print("random letter: "+random_let)
+
+  rand_index = randint(0, len(data['alphabet'])-1)
+  #return data[rand_index]['alphabet']
+  return data['alphabet'][rand_index]
+
+#example for sound data['alphabet'][rand_index]['sound']
+
+def return_json():
+  return data
