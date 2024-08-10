0484d510b9070f388b50067efab2d75b63d212c0
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2024-08-10 13:19:00 -0400

    Update EnglishWords.js

--- client/src/components/EnglishWords.js
+++ client/src/components/EnglishWords.js
@@ -12,12 +12,12 @@ function EnglishWords() {
         const record = await pb.collection('alphabet').getOne('5xj955bygaik9yg');
         
         // Randomly select a letter from the alphabet array
-        const randInt = Math.floor(Math.random() * record.json.alphabet.length);
+        const randInt = Math.floor(Math.random() * record.json.words.length);
         
-        setWord(record.json.alphabet[randInt].word);
-        setArabic(record.json.alphabet[randInt].arabic);
+        setWord(record.json.words[randInt].word);
+        setArabic(record.json.words[randInt].arabic);
 
-        console.log(record.json.alphabet[randInt].letter);
+        console.log(record.json.words[randInt].letter);
       } catch (error) {
         console.error("Error fetching data:", error);
         console.error("Error:", error.message, error.stack);
